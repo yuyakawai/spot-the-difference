471681fc98d9a7c24d27a2d51d347f5b09124cc6
codecommit-test-user <register@yuyakawai.com>
2024-09-01 15:52:40 +0900

    Add game parameters and status properties

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const gameParameters = {
+  maxQuestionNumber: 2,
   initialRemainingTime: 60,
-  cellRow: 4,
-  cellCol: 4,
 };
 
 const gameStatus = {
@@ -11,6 +10,9 @@ const gameStatus = {
   isGameOver: false,
   startTime: 0,
   remainingTime: 0,
+  questionNumber: 0,
+  character: null,
+  dummyCharacter: null,
 };
 
 const mainContainer = {
@@ -25,11 +27,28 @@ const screenContainer = {
   height: mainContainer.height - 10,
 };
 
-const timeMessageContainer = {
+const statusMessageContainer = {
   element: null,
   width: screenContainer.width,
   height: screenContainer.height * 0.1,
 };
+
+const characterData = [
+  {
+    level: 1,
+    character: "肉",
+    dummyCharacter: "内",
+  },
+  // {
+  //   level: 1,
+  //   character: "明",
+  //   dummyCharacter: "朋",
+  // },
+];
+
+const cellRow = 8;
+const cellCol = 11;
+const cellSize = screenContainer.width / cellRow;
 
 window.onload = () => {
   init();
@@ -59,100 +78,114 @@ const init = () => {
   screenContainer.element.style.position = "relative";
   screenContainer.element.style.width = screenContainer.width + "px";
   screenContainer.element.style.height = screenContainer.height + "px";
-  screenContainer.element.style.margin = "1px";
+  screenContainer.element.style.margin = "3px";
   screenContainer.element.style.display = "flex";
   screenContainer.element.style.alignItems = "center";
   screenContainer.element.style.justifyContent = "center";
   mainContainer.element.appendChild(screenContainer.element);
 
-  timeMessageContainer.element = document.createElement("div");
-  timeMessageContainer.element.style.position = "relative";
-  timeMessageContainer.element.style.width = timeMessageContainer.width + "px";
-  timeMessageContainer.element.style.height =
-    timeMessageContainer.height + "px";
-  timeMessageContainer.element.style.margin = "1px";
-  timeMessageContainer.element.style.fontSize = "20px";
-  timeMessageContainer.element.textContent =
+  statusMessageContainer.element = document.createElement("div");
+  statusMessageContainer.element.style.position = "relative";
+  statusMessageContainer.element.style.width =
+    statusMessageContainer.width + "px";
+  statusMessageContainer.element.style.height =
+    statusMessageContainer.height + "px";
+  statusMessageContainer.element.style.margin = "1px";
+  statusMessageContainer.element.style.fontSize = "20px";
+  statusMessageContainer.element.textContent =
     "⌛ " + gameParameters.initialRemainingTime.toFixed(2);
-  mainContainer.element.appendChild(timeMessageContainer.element);
+  mainContainer.element.appendChild(statusMessageContainer.element);
 
-  cells.forEach((cell) => cell.init());
+  initQuestion();
 
   tick();
 };
 
 const tick = () => {
+  if (gameStatus.isGameClear || gameStatus.isGameOver) {
+    return;
+  }
+
   gameStatus.remainingTime = Math.max(
     0,
     gameParameters.initialRemainingTime -
       (performance.now() - gameStatus.startTime) / 1000
   );
 
-  timeMessageContainer.element.textContent =
-    "⌛ " + gameStatus.remainingTime.toFixed(2);
+  statusMessageContainer.element.textContent =
+    "⌛ " +
+    gameStatus.remainingTime.toFixed(2) +
+    " " +
+    "問題 " +
+    gameStatus.questionNumber +
+    "/" +
+    gameParameters.maxQuestionNumber;
 
   if (gameStatus.remainingTime <= 0) {
     gameStatus.isGameOver = true;
     showGameOverMessage();
+    return;
   }
   requestAnimationFrame(tick);
 };
 
-const cells = [...Array(gameParameters.cellRow * gameParameters.cellCol)].map(
-  (_, index) => {
-    return {
-      element: null,
-      number: index + 1,
-      isEmpty: false,
-      x: 0,
-      y: 0,
-      init: () => {
-        cells[index].x = index % cellRow;
-        cells[index].y = Math.trunc(index / cellRow);
-        cells[index].element = document.createElement("div");
-        cells[index].element.style.position = "absolute";
-        cells[index].element.style.width = cellSize + "px";
-        cells[index].element.style.height = cellSize + "px";
-        cells[index].element.style.left = cells[index].x * cellSize + "px";
-        cells[index].element.style.top = cells[index].y * cellSize + "px";
-        cells[index].element.style.border = "3px ridge #cb986f";
-        cells[index].element.style.backgroundColor = "#ccb28e";
-        cells[index].element.style.boxSizing = "border-box";
-        cells[index].element.style.fontSize = cellSize * 0.6 + "px";
-        cells[index].element.style.display = "flex";
-        cells[index].element.style.alignItems = "center";
-        cells[index].element.style.justifyContent = "center";
-        cells[index].element.style.cursor = "pointer";
-        cells[index].element.textContent = cells[index].number;
-        screenContainer.element.appendChild(cells[index].element);
-
-        if (index === cells.length - 1) {
-          cells[index].isEmpty = true;
-        }
-
-        const handleEvent = (selfObject) => {
-          return (e) => {
-            e.preventDefault();
-            if (
-              gameStatus.isGameStart === false ||
-              gameStatus.isGameClear ||
-              gameStatus.isGameOver
-            ) {
-              return;
-            }
-            swapCell(selfObject);
-          };
-        };
-
-        if (window.ontouchstart === null) {
-          cells[index].element.ontouchstart = handleEvent(cells[index]);
-        } else {
-          cells[index].element.onpointerdown = handleEvent(cells[index]);
-        }
-      },
-    };
+const initQuestion = () => {
+  if (gameStatus.questionNumber >= gameParameters.maxQuestionNumber) {
+    gameStatus.isGameClear = true;
+    showGameClearMessage();
+    return;
   }
-);
+  gameStatus.questionNumber++;
+
+  const char =
+    characterData[[Math.floor(Math.random() * characterData.length)]];
+  gameStatus.character = char.character;
+  gameStatus.dummyCharacter = char.dummyCharacter;
+
+  cells.forEach((cell) => cell.init());
+  cells[[Math.floor(Math.random() * cells.length)]].element.textContent =
+    gameStatus.character;
+};
+
+const cells = [...Array(cellRow * cellCol)].fill().map((_, index) => ({
+  element: null,
+  isEmpty: false,
+  x: 0,
+  y: 0,
+  init: () => {
+    cells[index].x = index % cellRow;
+    cells[index].y = Math.trunc(index / cellRow);
+    cells[index].element = document.createElement("div");
+    cells[index].element.style.position = "absolute";
+    cells[index].element.style.width = cellSize + "px";
+    cells[index].element.style.height = cellSize + "px";
+    cells[index].element.style.left = cells[index].x * cellSize + "px";
+    cells[index].element.style.top = cells[index].y * cellSize + "px";
+    cells[index].element.style.border = "3px ridge #cb986f";
+    cells[index].element.style.backgroundColor = "#ccb28e";
+    cells[index].element.style.boxSizing = "border-box";
+    cells[index].element.style.fontSize = cellSize * 0.6 + "px";
+    cells[index].element.style.display = "flex";
+    cells[index].element.style.alignItems = "center";
+    cells[index].element.style.justifyContent = "center";
+    cells[index].element.style.cursor = "pointer";
+    cells[index].element.textContent = gameStatus.dummyCharacter;
+    screenContainer.element.appendChild(cells[index].element);
+
+    const handleCellTouchEvent = (e) => {
+      e.preventDefault();
+      if (e.target.textContent === gameStatus.character) {
+        initQuestion();
+      }
+    };
+
+    if (window.ontouchstart === null) {
+      cells[index].element.ontouchstart = handleCellTouchEvent;
+    } else {
+      cells[index].element.onpointerdown = handleCellTouchEvent;
+    }
+  },
+}));
 
 const scene = [
   {
@@ -208,3 +241,35 @@ const drawGameClearMessage = () => {};
 const drawStageClearMessage = () => {};
 
 const resetGame = () => {};
+
+const showGameClearMessage = () => {
+  let messageElement = document.createElement("div");
+  messageElement.style.position = "relative";
+  messageElement.style.zIndex = "1";
+  messageElement.style.width = screenContainer.width * 0.8 + "px";
+  messageElement.style.height = screenContainer.height * 0.15 + "px";
+  messageElement.style.display = "flex";
+  messageElement.style.alignItems = "center";
+  messageElement.style.justifyContent = "center";
+  messageElement.style.backgroundColor = "#f5deb3";
+  messageElement.style.color = "blue";
+  messageElement.style.fontSize = "36px";
+  messageElement.textContent = "Game Clear !!";
+  screenContainer.element.appendChild(messageElement);
+};
+
+const showGameOverMessage = () => {
+  let messageElement = document.createElement("div");
+  messageElement.style.position = "relative";
+  messageElement.style.zIndex = "1";
+  messageElement.style.width = screenContainer.width * 0.8 + "px";
+  messageElement.style.height = screenContainer.height * 0.15 + "px";
+  messageElement.style.display = "flex";
+  messageElement.style.alignItems = "center";
+  messageElement.style.justifyContent = "center";
+  messageElement.style.backgroundColor = "#f5deb3";
+  messageElement.style.color = "red";
+  messageElement.style.fontSize = "32px";
+  messageElement.textContent = "Game Over";
+  screenContainer.element.appendChild(messageElement);
+};
